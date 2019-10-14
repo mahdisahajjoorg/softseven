35672c50b906334c901375b4d6dd1bd56ecda6a9
Sajol Mahmud <sajol@sahajjo.com>
2019-10-14 10:27:29 +0600

    Some updates on softseven and apis

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,17 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Api
+Route::group(['prefix'=>'client','namespace'=>'api'], function(){
+	//Schools Api
+	Route::get('requestAllSchool', 'apiSchoolController@getAllSchoolName');
+	Route::get('check_school', 'apiSchoolController@getSchool');
+	Route::get('fastcodenew', 'apiSchoolController@fastcodenew');
+
+	//Student Api
+	Route::get('request_fastcode', 'ApiStudentController@getStudent');
+	Route::get('request_screenname', 'ApiStudentController@request_screenname');
+
+});
